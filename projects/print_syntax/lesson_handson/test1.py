20730495217f9a60d75da6801bc00be11b349eea
John Carlo Aguilar <johncarlo.aguilar104@gmail.com>
2026-09-21 21:01:13 +0800

    lesson test

---
+++ projects/print_syntax/lesson_handson/test1.py
@@ -0,0 +1,54 @@
+ch = "|"
+
+empno = 7369
+ename = 'SMITH'
+job = 'CLERK'
+sal = 800
+print(empno, ename, job, sal, sep=ch)
+
+empno = 7499
+ename = 'ALLEN'
+job = 'SALESMAN'
+sal = 1600
+print(empno, ename, job, sal, sep=ch)
+
+print("\n=========================================================")
+
+ch = '|'
+empno = 7369
+ename = 'SMITH'
+job = 'CLERK'
+sal = 800
+print(empno, ename, job, sal, sep=ch)
+empno = 7499
+ename = 'ALLEN'
+job = 'SALESMAN'
+sal = 1600
+print(empno, ename, job, sal, sep=ch)
+
+
+print("\n=========================================================")
+
+ch = '|'
+print('Empno','EmpName','Job','Salary','Hiredate','Address', sep=ch)
+empno = 7369
+ename = 'SMITH'
+job = 'CLERK'
+sal = 800
+hiredate = '2003-06-13'
+address = 'Santa Mesa, Manila'
+print(empno, ename, job, sal,hiredate, address, sep=ch)
+empno = 7499
+ename = 'ALLEN'
+job = 'SALESMAN'
+sal = 1600
+hiredate = '2003-08-15'
+address = 'Diliman, Quezon City'
+print(empno, ename, job, sal, hiredate, address, sep=ch)
+empno = 7521
+ename = 'WARD'
+job = ''
+sal = 1250
+hiredate='2004-03-26'
+address = 'Bangkal, Makati'
+print(empno, ename, job, sal, hiredate, address, sep=ch)
